c7b573e441bebd68b082636a54c00a13541a8a12
yuta720 <mryy720@yahoo.co.jp>
2021-10-24 14:16:46 +0000

    アイコン追加

--- components/layouts/Appbar.vue
+++ components/layouts/Appbar.vue
@@ -1,6 +1,13 @@
 <template>
   <v-app-bar dark app clipped-left>
     <v-app-bar-title>Cram School</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <v-btn class="mx-2" icon>
+      <v-icon>mdi-bell</v-icon>
+    </v-btn>
+    <v-btn class="mr-4" icon>
+      <v-icon>mdi-account</v-icon>
+    </v-btn>
   </v-app-bar>
 </template>
 
